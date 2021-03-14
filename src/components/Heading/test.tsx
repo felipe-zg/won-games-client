b4efaeeb79a599534d5af93be1335d1af026fac0
felipe-zg <felipe_zeba@outlook.com>
2021-03-14 11:54:23 -0300

    test: ensure Heading component renders a white heading by default

---
+++ src/components/Heading/test.tsx
@@ -0,0 +1,13 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/test/helpers'
+
+import Heading from '.'
+
+describe('<Heading />', () => {
+  it('should render a white heading by default', () => {
+    renderWithTheme(<Heading>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+})

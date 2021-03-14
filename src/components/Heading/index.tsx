19cfb8be285c51af37fd4c32d934d8cda070a0d5
felipe-zg <felipe_zeba@outlook.com>
2021-03-14 11:20:24 -0300

    feat: add lineLeft property to display a blue border left

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -2,10 +2,11 @@ import * as S from './styles'
 
 export type HeadingProps = {
   children: React.ReactNode
+  lineLeft?: boolean
 }
 
-const Heading = ({ children }: HeadingProps) => (
-  <S.Wrapper>{children}</S.Wrapper>
+const Heading = ({ children, lineLeft = false }: HeadingProps) => (
+  <S.Wrapper lineLeft={lineLeft}>{children}</S.Wrapper>
 )
 
 export default Heading
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,12 +1,22 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
+import { HeadingProps } from '.'
 
-export const Wrapper = styled.h2`
-  ${({ theme }) => css`
+const WrapperModifiers = {
+  lineLeft: (theme: DefaultTheme) => css`
+    border-left: 0.7rem solid ${theme.colors.secondary};
+    padding-left: ${theme.spacings.xxsmall};
+  `
+}
+
+export const Wrapper = styled.h2<HeadingProps>`
+  ${({ theme, lineLeft }) => css`
     font-size: ${theme.font.sizes.xlarge};
 
     ${media.greaterThan('medium')`
       font-size: ${theme.font.sizes.xxlarge};
     `}
+
+    ${lineLeft && WrapperModifiers.lineLeft(theme)}
   `}
 `

dbab591750aae2ce7129f1abc5577a191e998b46
felipe-zg <felipe_zeba@outlook.com>
2021-03-15 18:49:23 -0300

    test: ensure it shows content based on the media passed

--- src/components/MediaMatch/test.tsx
+++ src/components/MediaMatch/test.tsx
@@ -26,4 +26,13 @@ describe('<MediaMatch />', () => {
     expect(desktopHeading.parentElement).toHaveStyleRule('display', 'none')
     expect(mobileHeading.parentElement).toHaveStyleRule('display', 'none')
   })
+
+  it('should show based on the media passed', () => {
+    expect(desktopHeading.parentElement).toHaveStyleRule('display', 'block', {
+      media: '(min-width: 768px)'
+    })
+    expect(mobileHeading.parentElement).toHaveStyleRule('display', 'block', {
+      media: '(max-width: 768px)'
+    })
+  })
 })

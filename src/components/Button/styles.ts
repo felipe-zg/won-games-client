9bd7e48b2e9f10d47ac1a25b91ee5889375d9271
felipe-zg <felipe_zeba@outlook.com>
2021-03-15 20:21:34 -0300

    feat: ensure it renders a button with 100% width if fullWidth property is passed

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,7 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import { ButtonProps } from '.'
 
-type WrapperProps = Pick<ButtonProps, 'size'>
+type WrapperProps = Pick<ButtonProps, 'size' | 'fullWidth'>
 
 const WrapperModifiers = {
   small: (theme: DefaultTheme) => css`
@@ -19,16 +19,21 @@ const WrapperModifiers = {
     height: 5rem;
     font-size: ${theme.font.sizes.medium};
     padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+  `,
+
+  fullWidth: () => css`
+    width: 100%;
   `
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size }) => css`
+  ${({ theme, size, fullWidth }) => css`
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: 0;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
     ${!!size && WrapperModifiers[size!](theme)}
+    ${fullWidth && WrapperModifiers.fullWidth}
   `}
 `

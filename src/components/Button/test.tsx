d553b537e7ebdcff1c710602743058283061a4f1
felipe-zg <felipe_zeba@outlook.com>
2021-03-17 15:18:46 -0300

    refactor: add test snapshot

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -7,12 +7,14 @@ import Button from '.'
 
 describe('<Button />', () => {
   it('should render a medium sized button by default', () => {
-    renderWithTheme(<Button>Buy Now</Button>)
+    const { container } = renderWithTheme(<Button>Buy Now</Button>)
     expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
       'font-size': '1.4rem'
     })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render a small button if small is passed as size property', () => {
